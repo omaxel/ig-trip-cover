0e9b2658a38b4097a23dc9a6ec7b65f2399015ff
Omar Muscatello <7016897+omaxel@users.noreply.github.com>
2025-09-30 10:36:24 +0200

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImageEditorComponent } from "./image-editor/image-editor";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ImageEditorComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/image-editor/image-editor.ts
@@ -0,0 +1,61 @@
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild, ElementRef, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import html2canvas from 'html2canvas';
+import { isHeic, heicTo } from "heic-to"
+
+@Component({
+    selector: 'app-image-editor',
+    templateUrl: './image-editor.html',
+    styleUrls: ['./image-editor.scss'],
+    imports: [FormsModule, CommonModule]
+})
+export class ImageEditorComponent {
+    status$ = signal('');
+    imageSrc$ = signal<null | string>(null);
+    overlayText: string = 'Your Text Here';
+    bgOpacity = signal(0.65);
+    exporting$ = signal(false);
+
+    @ViewChild('canvasContainer') canvasContainer!: ElementRef;
+
+    async onFileSelected(event: Event) {
+        const file = (event.target as HTMLInputElement).files?.[0];
+
+        if (!file) {
+            return;
+        }
+
+        if (await isHeic(file)) {
+            this.status$.set('Converting HEIC to JPEG...');
+            const jpeg = await heicTo({
+                blob: file,
+                type: "image/jpeg",
+                quality: 0.85
+            })
+            this.imageSrc$.set(URL.createObjectURL(jpeg));
+            this.status$.set('Image loaded.');
+            return;
+        }
+
+        const reader = new FileReader();
+        reader.onload = () => this.imageSrc$.set(reader.result as string);
+        reader.readAsDataURL(file);
+        this.status$.set('Image loaded.');
+    }
+
+    downloadImage(): void {
+        this.exporting$.set(true);
+
+        html2canvas(this.canvasContainer.nativeElement, {
+            width: 1080,
+            height: 1920
+        }).then(canvas => {
+            const link = document.createElement('a');
+            link.download = 'edited-image.jpg';
+            link.href = canvas.toDataURL('image/jpeg');
+            link.click();
+            this.exporting$.set(false);
+        });
+    }
+}
